4a31acca1363e4c73cd0f52ac20145b138940009
AF <gulabsinghrajput008@gmail.com>
2025-06-17 05:57:06 +0530

    Create hold.py

---
+++ hold.py
@@ -0,0 +1,13 @@
+import requests
+import time
+
+url = "https://grumpy-philippa-jagdish2584-f05ce748.koyeb.app"
+
+while True:
+    try:
+        response = requests.get(url)
+        print("Status Code:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)
+    
+    time.sleep(20)
