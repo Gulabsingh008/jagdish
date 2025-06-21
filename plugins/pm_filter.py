3e828b96a56ee42fa5f1d7c7d95e4c7100f8d7c2
AF <gulabsinghrajput008@gmail.com>
2025-06-21 12:49:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2324,7 +2324,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.DISCLAIMER,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
-    
+        )
     elif query.data == "sticker":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="help"),
